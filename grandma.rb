86792a01e29d6937fc02d42bdd8be4e49065a22b
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-23 20:19:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end 
 # Whatever you say to grandma, she should respond with
