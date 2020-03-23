6e52702d455c867f06fba2b2738e21578978c0ae
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-23 20:17:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if !phrase == phrase.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end 
